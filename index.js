39ce474f9888259b3f167808690250fe6435709d
elissamaine <emaine97@yahoo.com>
2023-03-24 22:10:25 -0700

    added questions to the prompt

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./generateMarkdown');
+
 
 inquirer
   .prompt([
     {
-
+      type: 'input',
+      name: 'title',
+      message: "What's the title of your project?",
     },
     {
-
+      type: 'input',
+      name: 'description',
+      message: "What's the descrption of your project?",
     },
-
+    {
+      type: 'input',
+      name: 'install',
+      message: "What are the required steps to install your project?",
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: "what's the project's usage information?",
+    },
+    {
+      type: 'list',
+      name: 'liscence',
+      message: "Liscence?",
+      choices: []
+    },
 
   ])
 
@@ -18,7 +39,7 @@ inquirer
     const fileName = `${data.name.toLowerCase().split(' ').join('')}README.md`;
 
     fs.writeFile(fileName, JSON.stringify(data, null, '\t'), (err) =>
-      err ? console.log(err) : console.log('Success!')
+      err ? console.log(err) : console.log('README created!')
     );
   });
 
