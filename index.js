01b5d1f340b194ea135ebe7615563c08358dcb26
elissamaine <emaine97@yahoo.com>
2023-03-24 20:51:27 -0700

    added inquirer prompt outline to index.js

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+inquirer
+  .prompt([
+    {
+
+    },
+    {
+
+    },
+
+
+  ])
+
+  .then((fileName, data) => {
+    const fileName = `${data.name.toLowerCase().split(' ').join('')}README.md`;
+
+    fs.writeFile(fileName, JSON.stringify(data, null, '\t'), (err) =>
+      err ? console.log(err) : console.log('Success!')
+    );
+  });
 
 // TODO: Create an array of questions for user input
-const questions = [];
+//const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+//function init() {}
 
 // Function call to initialize app
-init();
+//init();
